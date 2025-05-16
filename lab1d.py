5a4b5f61803a514ad54c3649b722429f8d26844b
Urmil patel <urpatel5@myseneca.ca>
2025-05-16 12:10:35 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Assign integer values
+x = 10
+y = 2
+z = 5
+
+# Perform the math operation
+result = x + y * z
+
+# Print formatted result
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(result))
